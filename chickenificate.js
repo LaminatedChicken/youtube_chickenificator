86f77843dbb97b5a24b1b50b7e2d893a0b524147
AyOhEe <74308087+AyOhEe@users.noreply.github.com>
2024-04-14 00:41:55 +0100

    Rearranged to prepare for edge case addressing

--- chickenificate.js
+++ chickenificate.js
@@ -1,14 +1,25 @@
-//icon replacement
-let oldIcon = $("yt-icon#logo-icon")[0];
-let parent = oldIcon.parentElement;
-oldIcon.remove();
+//-----icon replacement-----
+if ($("yt-icon#logo-icon").length != 0) {
+    //regular replacement
 
-let newIcon = document.createElement("img");
-newIcon.src = browser.runtime.getURL("yt_wordmark_header_dark.png");
-newIcon.width = 81;
-newIcon.height = 30;
-parent.appendChild(newIcon);
+    //remove old icon and store parent reference
+    let oldIcon = $("yt-icon#logo-icon")[0];
+    let parent = oldIcon.parentElement;
+    console.log(parent);
+    oldIcon.remove();
+
+    //create new icon, getting a link to the the locally stored icon
+    let newIcon = document.createElement("img");
+    newIcon.src = browser.runtime.getURL("yt_wordmark_header_dark.png");
+    newIcon.width = 81;
+    newIcon.height = 30;
+    parent.appendChild(newIcon);
+    
+    console.log("Chickenify: attempted regular icon replacement");
+}
 
 
-//"GB" --> "CKN"
-$("span#country-code")[0].innerHTML = "CKN";
+//-----"GB" --> "CKN"-----
+if ($("span#country-code").length != 0){
+    $("span#country-code")[0].innerHTML = "CKN";
+}
