566d5789e221c1b14dbab439f395b49bd8ff07c4
AyOhEe <74308087+AyOhEe@users.noreply.github.com>
2024-04-14 00:10:52 +0100

    Image replacement

--- chickenificate.js
+++ chickenificate.js
@@ -1,1 +1,9 @@
-$("yt-icon#logo-icon").remove();
+let oldIcon = $("yt-icon#logo-icon")[0];
+let parent = oldIcon.parentElement;
+oldIcon.remove();
+
+let newIcon = document.createElement("img");
+newIcon.src = browser.runtime.getURL("yt_wordmark_header_dark.png");
+newIcon.width = 81;
+newIcon.height = 30;
+parent.appendChild(newIcon);
