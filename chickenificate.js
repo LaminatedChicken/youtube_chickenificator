49cfa583eba54bba9ee5be46485d2f13fd475f5c
AyOhEe <74308087+AyOhEe@users.noreply.github.com>
2024-04-14 13:25:57 +0100

    YouTube Doodle replacement

--- chickenificate.js
+++ chickenificate.js
@@ -1,13 +1,26 @@
 //-----icon replacement-----
-if ($("yt-icon#logo-icon").length != 0) {
+let parent = null;
+if ($("img.ytd-yoodle-renderer").length != 0) {
+    //doodle replacement
+
+    //remove old icon and store parent reference
+    let oldIcon = $("img.ytd-yoodle-renderer")[0]
+    parent = oldIcon.parentElement;
+    console.log(parent);
+    oldIcon.remove();
+}
+
+if (parent === null && $("yt-icon#logo-icon").length != 0) {
     //regular replacement
 
     //remove old icon and store parent reference
     let oldIcon = $("yt-icon#logo-icon")[0];
-    let parent = oldIcon.parentElement;
+    parent = oldIcon.parentElement;
     console.log(parent);
     oldIcon.remove();
+}
 
+if (parent != null) {
     //create new icon, getting a link to the the locally stored icon
     let newIcon = document.createElement("img");
     newIcon.src = browser.runtime.getURL("yt_wordmark_header_dark.png");
@@ -15,7 +28,7 @@ if ($("yt-icon#logo-icon").length != 0) {
     newIcon.height = 30;
     parent.appendChild(newIcon);
     
-    console.log("Chickenify: attempted regular icon replacement");
+    console.log("Chickenify: attempted icon replacement");
 }
 
 
